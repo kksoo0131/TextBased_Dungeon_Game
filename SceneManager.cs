07d29b880d0fa819b80396baa4bc0b476e2522ab
kksoo0131 <a67636521@gmail.com>
2023-08-22 13:02:54 +0900

    reflection 사용

--- SceneManager.cs
+++ SceneManager.cs
@@ -8,10 +8,19 @@
         {
             for (int i =0; i< (int)SceneType.EndPoint; i++)
             {
+                SceneType selectedScene = (SceneType)i;
+
+                string sceneName = Enum.GetName(selectedScene);
+
+                Type type = Type.GetType($"TextBased_Dungeon_Game.{sceneName}");
+                
+                Scene instance = (Scene)Activator.CreateInstance(type);
+                // Activator로 객체를 생성하려면 default 생성자가 있어야함
+                _scenes.Add(instance);
                 // Enum이름으로 Type을 구분해서 List에 추가
                 // Reflection 사용법 익히기
             }
-            _scenes.Add(new StartScene(SceneType.StartScene));
+            /*_scenes.Add(new StartScene(SceneType.StartScene));
 
             _scenes.Add(new StatusScene(SceneType.StatusScene));
             _scenes.Add(new InventoryScene(SceneType.InventoryScene));
@@ -23,7 +32,7 @@
             _scenes.Add(new BuyScene(SceneType.BuyScene));
             _scenes.Add(new SellScene(SceneType.SellScene));
             _scenes.Add(new DungeonClearScene(SceneType.DungeonClearScene));
-            _scenes.Add(new DungeonFailScene(SceneType.DungeonFailScene));
+            _scenes.Add(new DungeonFailScene(SceneType.DungeonFailScene));*/
 
 
         }
